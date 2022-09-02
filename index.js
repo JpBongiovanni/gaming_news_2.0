28288e22ae63791b161879baed8125275203069e
Jason Bongiovanni <jpbon@protonmail.com>
2022-09-02 16:39:47 -0400

    fixes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const PORT = process.env.PORT || 8000;
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
+const { response } = require('express')
 
 const { get } = axios;
 const app = express();
